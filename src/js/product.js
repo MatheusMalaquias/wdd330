3c66328e7a69dcd123150eedf72e49973e986fb6
Your Name <you@your-domain.com>
2026-05-09 15:20:00 -0300

    Updated

--- src/js/product.js
+++ src/js/product.js
@@ -1,18 +1,9 @@
-import { setLocalStorage } from "./utils.mjs";
-import ProductData from "./ProductData.mjs";
-
-const dataSource = new ProductData("tents");
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
+  let cartItems = getLocalStorage("so-cart") || [];
 
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+  cartItems.push(product);
+
+  setLocalStorage("so-cart", cartItems);
+}
